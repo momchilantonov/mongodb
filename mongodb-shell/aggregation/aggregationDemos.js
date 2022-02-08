02daa0fd49abc9f923d780412fb87a4fce8ba045
momchilantonov <eng.antonov@gmail.com>
2022-02-08 23:20:41 +0200

    aggregate - $unwind

--- mongodb-shell/aggregation/aggregationDemos.js
+++ mongodb-shell/aggregation/aggregationDemos.js
@@ -179,139 +179,150 @@ db.persons.aggregate([
 // Shortcut for transforming date
 db.persons.aggregate([
     {
-      $project: {
-        _id: 0,
-        name: 1,
-        email: 1,
-        birthdate: { $toDate: '$dob.date' },
-        age: "$dob.age",
-        location: {
-          type: 'Point',
-          coordinates: [
-            {
-              $convert: {
-                input: '$location.coordinates.longitude',
-                to: 'double',
-                onError: 0.0,
-                onNull: 0.0
-              }
-            },
-            {
-              $convert: {
-                input: '$location.coordinates.latitude',
-                to: 'double',
-                onError: 0.0,
-                onNull: 0.0
-              }
+        $project: {
+            _id: 0,
+            name: 1,
+            email: 1,
+            birthdate: { $toDate: '$dob.date' },
+            age: "$dob.age",
+            location: {
+                type: 'Point',
+                coordinates: [
+                    {
+                        $convert: {
+                            input: '$location.coordinates.longitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    },
+                    {
+                        $convert: {
+                            input: '$location.coordinates.latitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    }
+                ]
             }
-          ]
         }
-      }
     },
     {
-      $project: {
-        gender: 1,
-        email: 1,
-        location: 1,
-        birthdate: 1,
-        age: 1,
-        fullName: {
-          $concat: [
-            { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
-            {
-              $substrCP: [
-                '$name.first',
-                1,
-                { $subtract: [{ $strLenCP: '$name.first' }, 1] }
-              ]
-            },
-            ' ',
-            { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
-            {
-              $substrCP: [
-                '$name.last',
-                1,
-                { $subtract: [{ $strLenCP: '$name.last' }, 1] }
-              ]
+        $project: {
+            gender: 1,
+            email: 1,
+            location: 1,
+            birthdate: 1,
+            age: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.first',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.first' }, 1] }
+                        ]
+                    },
+                    ' ',
+                    { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.last',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.last' }, 1] }
+                        ]
+                    }
+                ]
             }
-          ]
         }
-      }
     }
-  ]).pretty();
+]).pretty();
 
-  // $isoWeekYear
-  db.persons.aggregate([
+// $isoWeekYear
+db.persons.aggregate([
     {
-      $project: {
-        _id: 0,
-        name: 1,
-        email: 1,
-        birthdate: { $toDate: '$dob.date' },
-        age: "$dob.age",
-        location: {
-          type: 'Point',
-          coordinates: [
-            {
-              $convert: {
-                input: '$location.coordinates.longitude',
-                to: 'double',
-                onError: 0.0,
-                onNull: 0.0
-              }
-            },
-            {
-              $convert: {
-                input: '$location.coordinates.latitude',
-                to: 'double',
-                onError: 0.0,
-                onNull: 0.0
-              }
+        $project: {
+            _id: 0,
+            name: 1,
+            email: 1,
+            birthdate: { $toDate: '$dob.date' },
+            age: "$dob.age",
+            location: {
+                type: 'Point',
+                coordinates: [
+                    {
+                        $convert: {
+                            input: '$location.coordinates.longitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    },
+                    {
+                        $convert: {
+                            input: '$location.coordinates.latitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    }
+                ]
             }
-          ]
         }
-      }
     },
     {
-      $project: {
-        gender: 1,
-        email: 1,
-        location: 1,
-        birthdate: 1,
-        age: 1,
-        fullName: {
-          $concat: [
-            { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
-            {
-              $substrCP: [
-                '$name.first',
-                1,
-                { $subtract: [{ $strLenCP: '$name.first' }, 1] }
-              ]
-            },
-            ' ',
-            { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
-            {
-              $substrCP: [
-                '$name.last',
-                1,
-                { $subtract: [{ $strLenCP: '$name.last' }, 1] }
-              ]
+        $project: {
+            gender: 1,
+            email: 1,
+            location: 1,
+            birthdate: 1,
+            age: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.first',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.first' }, 1] }
+                        ]
+                    },
+                    ' ',
+                    { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.last',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.last' }, 1] }
+                        ]
+                    }
+                ]
             }
-          ]
         }
-      }
     },
     { $group: { _id: { birthYear: { $isoWeekYear: "$birthdate" } }, numPersons: { $sum: 1 } } },
     { $sort: { numPersons: -1 } }
-  ]).pretty();
+]).pretty();
 
-  // Pushing Elements Into Newly Created Arrays ($push)
-  db.personsDemos.aggregate([
-      {
+// Pushing Elements Into Newly Created Arrays ($push)
+db.personsDemos.aggregate([
+    {
         $group: {
-            _id: { age: '$age'},
-            allHobbies: { $push: '$hobbies'}
+            _id: { age: '$age' },
+            allHobbies: { $push: '$hobbies' }
         }
-      }
-  ]);
+    }
+]);
+
+// $unwind Stage -> create document for every element from the array (hobbies)
+db.personsDemos.aggregate([
+    { $unwind: "$hobbies" },
+    {
+        $group: {
+            _id: { age: "$age" },
+            allHobbies: { $push: "$hobbies" }
+        }
+    }
+]).pretty();
