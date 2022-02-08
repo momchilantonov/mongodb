1dac6a3ca5af978f3a75733a379c343e597583d6
momchilantonov <eng.antonov@gmail.com>
2022-02-08 22:40:45 +0200

    aggregate - Transform data to geoJSON

--- mongodb-shell/aggregation/aggregationDemos.js
+++ mongodb-shell/aggregation/aggregationDemos.js
@@ -48,3 +48,65 @@ db.persons.aggregate([
         }
     }
 ]).pretty();
+
+// Transform some date -> geoJSON (location)
+// id field is need only on the firs project
+db.persons.aggregate([
+    {
+        $project: {
+            _id: 0,
+            name: 1,
+            email: 1,
+            location: {
+                type: 'Point',
+                coordinates: [
+                    {
+                        $convert: {
+                            input: '$location.coordinates.longitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    },
+                    {
+                        $convert: {
+                            input: '$location.coordinates.latitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    },
+                ]
+            }
+        }
+    },
+    {
+        $project: {
+            gender: 1,
+            email: 1,
+            location: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.first',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.first' }, 1] }
+                        ]
+                    },
+                    ' ',
+                    { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.last',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.last' }, 1] }
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+]).pretty();
+
