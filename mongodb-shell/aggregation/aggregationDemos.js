b33aed8fa0d1fcdf9350a3e80bd0424fb5eb6213
momchilantonov <eng.antonov@gmail.com>
2022-02-09 01:28:35 +0200

    aggregate - Applying multiple operations to array

--- mongodb-shell/aggregation/aggregationDemos.js
+++ mongodb-shell/aggregation/aggregationDemos.js
@@ -358,12 +358,21 @@ db.personsDemos.aggregate([
     }
 ]).pretty();
 
-//
+// $filter
 db.personsDemos.aggregate([
     {
-      $project: {
-        _id: 0,
-        scores: { $filter: { input: '$examScores', as: 'sc', cond: { $gt: ["$$sc.score", 60] } } }
-      }
+        $project: {
+            _id: 0,
+            scores: { $filter: { input: '$examScores', as: 'sc', cond: { $gt: ["$$sc.score", 60] } } }
+        }
     }
-  ]).pretty();
+]).pretty();
+
+//
+db.personsDemos.aggregate([
+    { $unwind: "$examScores" },
+    { $project: { _id: 1, name: 1, age: 1, score: "$examScores.score" } },
+    { $sort: { score: -1 } },
+    { $group: { _id: "$_id", name: { $first: "$name" }, maxScore: { $max: "$score" } } },
+    { $sort: { maxScore: -1 } }
+]).pretty();
