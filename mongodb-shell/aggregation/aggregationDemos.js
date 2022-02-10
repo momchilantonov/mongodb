b6769ae2518697a3911d86dd13cd4c493618a559
momchilantonov <eng.antonov@gmail.com>
2022-02-10 21:09:59 +0200

    aggregate - geoData in a pipline

--- mongodb-shell/aggregation/aggregationDemos.js
+++ mongodb-shell/aggregation/aggregationDemos.js
@@ -421,3 +421,84 @@ db.persons.aggregate([
     { $skip: 10 },
     { $limit: 10 }
 ]).pretty();
+
+// Write pipline result in a new collection ($out)
+db.persons.aggregate([
+    {
+        $project: {
+            _id: 0,
+            name: 1,
+            email: 1,
+            birthdate: { $toDate: '$dob.date' },
+            age: "$dob.age",
+            location: {
+                type: 'Point',
+                coordinates: [
+                    {
+                        $convert: {
+                            input: '$location.coordinates.longitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    },
+                    {
+                        $convert: {
+                            input: '$location.coordinates.latitude',
+                            to: 'double',
+                            onError: 0.0,
+                            onNull: 0.0
+                        }
+                    }
+                ]
+            }
+        }
+    },
+    {
+        $project: {
+            gender: 1,
+            email: 1,
+            location: 1,
+            birthdate: 1,
+            age: 1,
+            fullName: {
+                $concat: [
+                    { $toUpper: { $substrCP: ['$name.first', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.first',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.first' }, 1] }
+                        ]
+                    },
+                    ' ',
+                    { $toUpper: { $substrCP: ['$name.last', 0, 1] } },
+                    {
+                        $substrCP: [
+                            '$name.last',
+                            1,
+                            { $subtract: [{ $strLenCP: '$name.last' }, 1] }
+                        ]
+                    }
+                ]
+            }
+        }
+    },
+    { $out: "transformedPersons" }
+]).pretty();
+
+//
+db.transformedPersons.aggregate([
+    {
+        $geoNear: {
+            near: {
+                type: 'Point',
+                coordinates: [-18.4, -42.8]
+            },
+            maxDistance: 1000000,
+            num: 10,
+            query: { age: { $gt: 30 } },
+            distanceField: "distance"
+        }
+    }
+]).pretty();
