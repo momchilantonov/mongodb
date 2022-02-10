ee318aa77361703ebd3754fe7b8abdc92c200e65
momchilantonov <eng.antonov@gmail.com>
2022-02-10 20:23:20 +0200

    aggregate - $bucket

--- mongodb-shell/aggregation/aggregationDemos.js
+++ mongodb-shell/aggregation/aggregationDemos.js
@@ -368,11 +368,40 @@ db.personsDemos.aggregate([
     }
 ]).pretty();
 
-//
+// Applying multiple operations to Array
 db.personsDemos.aggregate([
     { $unwind: "$examScores" },
     { $project: { _id: 1, name: 1, age: 1, score: "$examScores.score" } },
     { $sort: { score: -1 } },
     { $group: { _id: "$_id", name: { $first: "$name" }, maxScore: { $max: "$score" } } },
     { $sort: { maxScore: -1 } }
+]).pretty();
+
+// $bucket
+db.persons
+    .aggregate([
+        {
+            $bucket: {
+                groupBy: '$dob.age',
+                boundaries: [18, 30, 40, 50, 60, 120],
+                output: {
+                    numPersons: { $sum: 1 },
+                    averageAge: { $avg: '$dob.age' }
+                }
+            }
+        }
+    ])
+    .pretty();
+
+db.persons.aggregate([
+    {
+        $bucketAuto: {
+            groupBy: '$dob.age',
+            buckets: 5,
+            output: {
+                numPersons: { $sum: 1 },
+                averageAge: { $avg: '$dob.age' }
+            }
+        }
+    }
 ]).pretty();
